cc6169f8fe3b50541c987d13f487f84f6d1d09e6
tosh2230 <rev.to12@gmail.com>
2022-04-06 17:51:35 +0900

    change as container image

--- templates/lambda/load_parquet_to_bigquery/app.py
+++ templates/lambda/load_parquet_to_bigquery/app.py
@@ -1,15 +1,29 @@
 import base64
 import json
 import logging
+import os
 import urllib
+from typing import IO, Tuple
+
+from boto3 import Session
 from boto3.resources.base import ServiceResource
+from botocore.config import Config
 from google.cloud import bigquery
 from google.oauth2 import service_account
-from typing import IO, Tuple
 
 # logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+endpoint = os.environ["ENDPOINT"]
+s3_endpoint_url = os.environ["S3_ENDPOINT_URL"]
+sm_endpoint_url = os.environ["SECRETS_MANAGER_ENDPOINT_URL"]
+aws_region = os.environ["AWS_REGION"]
+gcp_sa_secret_name = os.environ["GCP_SA_SECRET_NAME"]
+
+session = Session()
+s3_client: ServiceResource
+sm_client: ServiceResource
 
 
 class LambdaProcessor(object):
@@ -95,3 +109,31 @@ class LambdaProcessor(object):
         job.result()
         current_rows = bq_client.get_table(table=destination).num_rows
         return previous_rows, current_rows
+
+
+def lambda_handler(event, context) -> dict:
+    if endpoint == "localstack":
+        print("Start Testing with Localstack")
+        s3_client = session.resource(
+            service_name="s3",
+            endpoint_url=s3_endpoint_url,
+            config=Config()
+        )
+        sm_client = session.resource(
+            service_name="secretsmanager",
+            endpoint_url=sm_endpoint_url,
+            config=Config()
+        )
+    else:
+        s3_client = session.resource(service_name="s3")
+        sm_client = session.resource(service_name="secretsmanager", region_name=aws_region)
+
+    processor = LambdaProcessor(
+        event=event,
+        context=context,
+        s3_client=s3_client,
+        sm_client=sm_client,
+        gcp_sa_secret_name=gcp_sa_secret_name
+
+    )
+    return processor.main()
